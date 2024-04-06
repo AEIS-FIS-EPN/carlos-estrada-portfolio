9fac85a61a11835d22a63ef71f5e47eecad9e59d
C4RLOS <carlos.estrada01@epn.edu.ec>
2024-04-05 23:50:26 -0500

    Add of models and assets (Three JS)

---
+++ src/components/HomeInfo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HomeInfo = () => {
+  return (
+<div className="fixed right-0 top-20 mr-20 w-90 h-200 rounded-lg bg-black-500 items-center justify-center flex font-bold shadow-md text-center">
+  <p className="text-white">Welcome this is just the beginning,<br /> more coming soon!</p>
+</div>
+  )
+}
+
+export default HomeInfo
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,55 @@
 import { Canvas } from '@react-three/fiber'
 import React, { Suspense } from 'react'
 import Loader from '../components/Loader'
+import {Island} from '../models/Island'
+import HomeInfo from '../components/HomeInfo'
 
 const Home = () => {
+
+  const adjustIslandForScreenSize = () => {
+    let screenScale=null;
+    let screenPosition= [0, -6.5, -43.4];
+    let rotation = [0.1, 4.8 , 0]
+
+    if (window.innerWidth < 768) {
+      screenScale = [0.9, 0.9, 0.9];
+    } else {
+      screenScale = [1, 1, 1];
+    }
+
+    return [screenScale, screenPosition, rotation];
+  };
+
+  const [islandScale, islandPosition, islandRotation] = adjustIslandForScreenSize();
+
   return (
     <section className='w-full h-screen relative'>
       {/* <div className='absolute top-28 letf-0 right-0 z-10 flex items-center justify-center'>
         popup
-      </div> */}
+      </div> */
+      
+      
+      }
 
+   
+      <HomeInfo />
+      
       <Canvas
       className='w-full hscreen bg-transparent'
       camera={{near: 0.1, far: 2000}}
       >
         <Suspense fallback={<Loader />}>
-          
+          <directionalLight />
+          <ambientLight />
+          <pointLight />
+          <spotLight />
+          <hemisphereLight/>
+
+          <Island
+          position={islandPosition}
+          scale={islandScale}
+          rotation={islandRotation}
+          />
         </Suspense>
       </Canvas>
 
